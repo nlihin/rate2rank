b2c8e7490cb4f739dc76ed12da69755eaa80dd70
matan lange <matanlange88@gmail.com>
2023-06-15 13:36:10 +0300

    test2

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask_cors import CORS,cross_origin
 from flask_jwt_extended import JWTManager
 import datetime
 
-app = Flask(__name__,static_folder='/frontend/build',static_url_path='')
-cors = CORS(app, supports_credentials=True, allow_headers=["Content-Type", "Authorization"], methods=["GET", "POST"])
+app = Flask(__name__,static_folder='../frontend/build',static_url_path='')
+cors = CORS(app, origins='http://localhost:3000', supports_credentials=True, allow_headers=["Content-Type", "Authorization"], methods=["GET", "POST"])
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rank2rate.db'
