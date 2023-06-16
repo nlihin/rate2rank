08167faf431c9a2d1c9fbbc99c7fbdf871cb917a
matan lange <matanlange88@gmail.com>
2023-06-16 15:13:16 +0300

    d v1

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 import datetime
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='../frontend/build',static_url_path='')
 cors = CORS(app, origins='http://localhost:3000', supports_credentials=True, allow_headers=["Content-Type", "Authorization"], methods=["GET", "POST"])
 
 
--- frontend/src/routes/url.js
+++ frontend/src/routes/url.js
@@ -1,1 +1,1 @@
-export const BaseURL = "http://127.0.0.1:5000/";
+export const BaseURL = 'https://rate2rank-0d561bf6674a.herokuapp.com/';
